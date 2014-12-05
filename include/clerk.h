fe352e9fad438ae54ea92a568dc35c5415cd5e0c
deso <deso@devnull.com>
2014-12-05 22:05:37 +0000

    add SIGTERM and SIGINT support
    When clerk is killed by SIGTERM or SIGINT it performs no cleanup and exits the
    hard way. State such as the cursor visibility or the current console colors
    are not reset to their original values.
    This change adds a handler for SIGTERM and SIGINT. Arguably, a handler for
    SIGINT is not really required since Ctrl+C (the usual way to receive a SIGINT)
    is already taken care of by termbox. However, since there might be other ways
    to send this signal to clerk an exit handler is registered for it as well.

--- include/clerk.h
+++ include/clerk.h
@@ -57,12 +57,14 @@ typedef struct clrk_clerk {
   color_configuration_t *colors;
   unsigned width;
   unsigned height;
+  clrk_exit_func exit_func;
+  volatile bool exit_func_invoked;
 } clrk_clerk_t;
 
 /*
  * Initialize Clerk with default values and draws user interface.
  */
-void clrk_init(const char *json, const char *config);
+void clrk_init(const char *json, const char *config, clrk_exit_func exit_func);
 
 /*
  * Create/add a new project
@@ -101,8 +103,10 @@ void clrk_todo_info(void);
 
 /*
  * Start Clerk.
- * If this function returns then Clerk will end.
+ * If this function returns then Clerk will end. The return value indicates
+ * whether the exit function supplied during initialization lead to the exit
+ * or not.
  */
-void clrk_loop_normal(void);
+bool clrk_loop_normal(void);
 
 #endif
